86b165cc5b2be7ab0c761f0c39665e06103fcf96
liuwin7 <liuwin7@163.com>
2015-09-08 18:08:46 +0800

    Modify .podspec

---
+++ Example/Pods/Target Support Files/FTFoldableTableView/FTFoldableTableView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FTFoldableTableView : NSObject
+@end
+@implementation PodsDummy_FTFoldableTableView
+@end
---
+++ Example/Pods/Target Support Files/Pods-FTFoldableTableView_Tests/Pods-FTFoldableTableView_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FTFoldableTableView_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FTFoldableTableView_TestsVersionString[];
+
